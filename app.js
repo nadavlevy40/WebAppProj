a20043b64a2bfdcf9ed66619d756523be5f9f58d
ytal <ytalamsel@gmail.com>
2023-06-13 19:06:02 +0300

    first model, was not tested yet, only srturcutre

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const mongoose = require('mongoose')
+const products =require('./routes/productsroute')
+const newLocal = require('custom-env')  
+newLocal.env(process.env.NODE_ENV,'./config');
 
-app.listen(80);
+mongoose.connect(process.env.CONNECTION_STRING,
+    {useNewUrlParser:true,
+    useUnifiedTopology:true});
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.json());
+
+//app.set('view engine','ejs')
+//TODO - confirm below line to correct products file
+app.use('/products', productsroute);
+app.listen(process.env.PORT);
