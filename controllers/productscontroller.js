58efd08d6035a2d5506510d40361eac32e7a5139
Nadav levy <69619783+nadavlevy40@users.noreply.github.com>
2023-06-28 15:12:46 +0300

    edit productsconroller

--- controllers/productscontroller.js
+++ controllers/productscontroller.js
@@ -1,5 +1,8 @@
 const Productservice = require('../services/productsservice')
 
+const moment = require('moment');
+const Chart = require('chart.js');
+
 const createProduct = async (req,res) => {
   const newProduct = await Productservice.createProduct(req.body.name,image,video,descrpition,category,amountInInventory,supplier);
   res.json(newProduct)
@@ -10,6 +13,32 @@ const getProducts = async (req,res) => {
   const Products = await Productservice.getProducts();
   res.json(Products);
 }
+
+const getMonthlyPurchaseData = async (req, res) => {
+  try {
+    const purchases = await Productservice.getProducts(); // Modify this line to retrieve the purchase records from the database
+
+   
+    const monthlyData = purchases.reduce((acc, purchase) => {
+      const month = moment(purchase.date).format('MMMM YYYY'); // Modify the 'date' field based on the data structure
+      if (!acc[month]) {
+        acc[month] = { total: 0, count: 0 };
+      }
+      acc[month].total += purchase.amount; // Modify the 'amount' field based on the data structure
+      acc[month].count += 1;
+      return acc;
+    }, {});
+
+    const monthlyAverage = Object.entries(monthlyData).map(([month, { total, count }]) => ({
+      month,
+      average: total / count,
+    }));
+
+    res.json(monthlyAverage);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while retrieving monthly purchase data.' });
+  }
+};
 
 const getProduct = async (req,res) => {
   const product = await Productservice.getProductById(req.params.id);
